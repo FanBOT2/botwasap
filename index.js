e296b064cde6f01899fe9cbaffde2200ed39ff3b
FanBOT2 <78063700+FanBOT2@users.noreply.github.com>
2021-01-27 15:36:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const  XBOT = 'AMPIBI'; // Nama Bot Whatsapp
-const instagram = 'https://instagram.com/affis_saputro123'; // Nama Instagramlu cok
-const nomer = 'https://Wa.me/+6282334297175'; // Nomor whatsapplu cok
-const aktif = 'Tergantung kuota'; // Kapan bot lu aktif
-const groupwa = 'comming soon'; // OFFICIAL GRUP LU 1
-const youtube = 'https://youtube.com/channel/UCYKxsg7iF9a9IZyXQRNsvqw'; 
+const  XBOT = 'FUNNYBOT'; // 
+const instagram = 'https://instagram.com/irfannnstore'; //
+const nomer = 'https://Wa.me/+6283875445000'; // 
+const aktif = 'Tergantung kuota'; //
+const groupwa = 'https://chat.whatsapp.com/Hxf0XTETWLu4quYbFWY2SS'; //  
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
